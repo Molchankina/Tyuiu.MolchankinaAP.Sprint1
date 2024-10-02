1e7bd9c116bed92d5decc1e99c66aa4c6d724e16
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-02 17:02:47 +0500

    Add project files.

---
+++ Tyuiu.MolchankinaAP.Sprint1.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.MolchankinaAP.Sprint1.Task0.V26.Lib;
+namespace Tyuiu.MolchankinaAP.Sprint1.Task0.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.MolchankinaAP.Sprint1.Task0.V26/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.MolchankinaAP.Sprint1.Task0.V26.Lib;
+namespace Tyuiu.MolchankinaAP.Sprint1.Task0.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Молчанкина А. П. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнила: Молчанкина Ангелина Павловна | СМАРТб-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* (4 * 5 / 2) - (18 / 2 / 3) и печатает результат на экране               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (4 * 5 / 2) - (18 / 2 / 3)                                              *");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
